45b231714c69356b44f88a6f540782222c8ebad8
goreleaserbot <goreleaser@carlosbecker.com>
2022-07-07 08:52:09 +0200

    Brew formula update for ns version v0.0.47

--- ns.rb
+++ ns.rb
@@ -5,20 +5,20 @@
 class Ns < Formula
   desc "Developer platform with a unified experience from development to production."
   homepage "https://namespacelabs.com/"
-  version "0.0.44"
+  version "0.0.47"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://ns-releases.s3.us-east-2.amazonaws.com/ns/v0.0.46/ns_0.0.46_darwin_arm64.tar.gz"
-      sha256 "e6c8f4cfdceace0a2b4e919b015d39ebea5b17814e57a79d15f8e0f1412d4a1d"
+      url "https://ns-releases.s3.us-east-2.amazonaws.com/ns/v0.0.47/ns_0.0.47_darwin_arm64.tar.gz"
+      sha256 "a64d0e4e2db3d6542437411c107020de4a6bf56370c6225428ea62c664233a88"
 
       def install
         bin.install "ns"
       end
     end
     if Hardware::CPU.intel?
-      url "https://ns-releases.s3.us-east-2.amazonaws.com/ns/v0.0.46/ns_0.0.46_darwin_amd64.tar.gz"
-      sha256 "97e28d2d3c5123dce22dd715da7c380cdfc3bc1d92e8467020714af317f8fd36"
+      url "https://ns-releases.s3.us-east-2.amazonaws.com/ns/v0.0.47/ns_0.0.47_darwin_amd64.tar.gz"
+      sha256 "c36d40f35a575967e655599e7fc6715196c1b7ac419e1983b257c8e225bb5531"
 
       def install
         bin.install "ns"
@@ -28,16 +28,16 @@ class Ns < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://ns-releases.s3.us-east-2.amazonaws.com/ns/v0.0.46/ns_0.0.46_linux_arm64.tar.gz"
-      sha256 "61307817dcd1d47aebe6a70f97683cc284a1c1b175aa868c1d5f58a7ae712e4f"
+      url "https://ns-releases.s3.us-east-2.amazonaws.com/ns/v0.0.47/ns_0.0.47_linux_arm64.tar.gz"
+      sha256 "e4ba7c84ff49124dd97843108f691af2837029cfe4353d5be031fb0a141200c7"
 
       def install
         bin.install "ns"
       end
     end
     if Hardware::CPU.intel?
-      url "https://ns-releases.s3.us-east-2.amazonaws.com/ns/v0.0.46/ns_0.0.46_linux_amd64.tar.gz"
-      sha256 "38b31a47ae75fc321f7b1b042f62f098623b5c73860270e4363d585f74d37b4a"
+      url "https://ns-releases.s3.us-east-2.amazonaws.com/ns/v0.0.47/ns_0.0.47_linux_amd64.tar.gz"
+      sha256 "7344fee83fb640b3aaa2b5ab73aa415548e7ec727881d929c4bf9a3399a3bef6"
 
       def install
         bin.install "ns"
